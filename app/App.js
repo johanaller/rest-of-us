270cc7e61272ee083d0a0f5db816f116e8aba5ff
johanaller <johan.berglund@aller.com>
2022-01-03 11:45:33 +0100

    Adding base routes

--- app/App.js
+++ app/App.js
@@ -3,14 +3,21 @@ import ReactDOM from "react-dom";
 import Header from "./src/components/Header";
 import Footer from "./src/components/Footer";
 import HomeGuest from "./src/components/HomeGuest";
+import Terms from "./src/components/Terms";
+import About from "./src/components/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
